3fe074715ed9c33af045f9d8e9aee23a11a9d2cd
petrvs66 <xlkang@mail.ustc.edu.cn>
2020-06-15 21:18:00 +0800

    feat: 简单实现node脚手架

---
+++ cli.js
@@ -0,0 +1,28 @@
+#!usr/bin/env node
+
+const path = require('path');
+const fs = require('fs')
+const inquirer = require('inquirer')
+const ejs = require('ejs')
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Project name?'
+    }
+])
+.then(answers => {
+    const tmplDir = path.join(__dirname, 'templates')
+    const destDir = process.cwd()
+
+    fs.readdir(tmplDir, (err, files)=>{
+        if(err) throw err;
+        files.forEach(file => {
+            ejs.renderFile(path.join(templDir, file), answers, (err, result) => {
+                if(err) throw err;
+                fs.writeFileSync(path.join(destDir, file), result)
+            })
+        })
+    })
+})
